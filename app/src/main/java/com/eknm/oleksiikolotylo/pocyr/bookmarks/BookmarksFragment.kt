90bafa168c0ed036a8471f7f08b407d4355a4d81
Oleksii Kolotylo <o.kolotylo@pnn.in.ua>
2023-02-08 22:53:33 +0800

    Add Navigation graph

---
+++ app/src/main/java/com/eknm/oleksiikolotylo/pocyr/bookmarks/BookmarksFragment.kt
@@ -0,0 +1,7 @@
+package com.eknm.oleksiikolotylo.pocyr.bookmarks
+
+import androidx.fragment.app.Fragment
+import com.eknm.oleksiikolotylo.pocyr.R
+
+class BookmarksFragment : Fragment(R.layout.fragment_bookmarks) {
+}
---
+++ app/src/main/java/com/eknm/oleksiikolotylo/pocyr/translate/TranslationFragment.kt
@@ -0,0 +1,7 @@
+package com.eknm.oleksiikolotylo.pocyr.translate
+
+import androidx.fragment.app.Fragment
+import com.eknm.oleksiikolotylo.pocyr.R
+
+class TranslationFragment : Fragment(R.layout.fragment_translation) {
+}
